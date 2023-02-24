eab79154fd454e31ebd782d5c37e4a14f15de31f
Lalit1550 <108258790+Lalit1550@users.noreply.github.com>
2023-02-24 15:45:22 +0530

    added

--- src/app/modules/purchase-requisition/pr-modal-view/pr-modal-view.component.ts
+++ src/app/modules/purchase-requisition/pr-modal-view/pr-modal-view.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { GridDataResult } from '@progress/kendo-angular-grid';
+import { SortDescriptor, process,  State } from '@progress/kendo-data-query';
+import { CommonService } from 'src/app/shared/common.service';
+import { PrGridDataDto } from '../pr-grid-view';
+import { PrGridData } from '../pr-overview/data';
 
 @Component({
   selector: 'app-pr-modal-view',
@@ -6,5 +11,63 @@ import { Component } from '@angular/core';
   styleUrls: ['./pr-modal-view.component.scss']
 })
 export class PrModalViewComponent {
+  public gridView: GridDataResult;
+  public state: State = {
+    sort: [
+      {
+        field: 'prid',
+        dir: 'asc',
+      },
+    ],
+    filter: {
+      logic: 'and',
+      filters: [],
+    },
+    skip: 0,
+    take: 10,
+  };
+  prData: PrGridDataDto[] = PrGridData;
 
+  columnWidth = 150;
+  pageSize = 10;
+
+  constructor(private commonService: CommonService) {}
+
+  public ngOnInit() {
+    this.loadProducts();
+  }
+
+  private loadProducts(): void {
+    this.gridView = process(this.prData, this.state);
+  }
+
+  checkMobileBrowser() {
+    console.log(
+      'this.commonService.isMobileBrowser',
+      this.commonService.isMobileBrowser
+    );
+    return this.commonService.isMobileBrowser;
+  }
+
+  editHandler(item: PrGridDataDto) {}
+
+  removeHandler(item: PrGridDataDto) {}
+
+  public onStateChange(state: any) {
+    this.state = state;
+
+    const newState: any = Object.assign({}, state);
+
+    const sortDescriptor = state.sort.find((s: any) => s.field === 'prid');
+    const newSortDescriptor: SortDescriptor = { field: '', dir: 'asc' };
+    if (sortDescriptor && sortDescriptor.field === 'prid') {
+      newSortDescriptor.field = 'Unit Price';
+      newSortDescriptor.dir = sortDescriptor.dir;
+    }
+    newState.sort = newState.sort.filter(
+      (s: any) => s.field !== 'ProdupridctName'
+    );
+    newState.sort.push(newSortDescriptor);
+    this.loadProducts();
+  }
 }
